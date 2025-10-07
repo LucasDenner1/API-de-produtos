62d3a2a15defa63b021233b43f17e4acbeadce82
Lucas Denner <lucas.denner14@hotmail.com>
2025-10-07 15:04:13 -0300

    first commit

---
+++ src/main/java/io/github/cursodsousa/produtosapi/model/Produto.java
@@ -0,0 +1,69 @@
+package io.github.cursodsousa.produtosapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+// não é obrigado
+@Table
+public class Produto {
+
+    // não é obrigado
+    @Id
+    @Column
+    private String id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String descricao;
+
+    @Column
+    private double preco;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+
+}
